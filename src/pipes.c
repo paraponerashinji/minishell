699b44334f535167c07d42b38e684235bf95b15e
paraponerashinji <aharder@student.42luxembourg.lu>
2025-03-02 13:27:18 +0100

    implemented heredoc

--- src/pipes.c
+++ src/pipes.c
@@ -6,7 +6,7 @@
 /*   By: aharder <aharder@student.42luxembourg.lu>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 11:28:02 by aharder           #+#    #+#             */
-/*   Updated: 2025/03/02 12:51:52 by aharder          ###   ########.fr       */
+/*   Updated: 2025/03/02 13:26:34 by aharder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -197,26 +197,51 @@ void	check_env(t_commands *temp)
 	}
 }
 
+void	get_heredoc(int *p_fd, char *end)
+{
+	char	*heredoc;
+
+	while (1)
+	{
+		heredoc = readline("> ");
+		if (ft_strcmp(heredoc, end) == 0)
+		{
+			free(heredoc);
+			break;
+		}
+		write(p_fd[1], heredoc, ft_strlen(heredoc));
+		free(heredoc);
+	}
+}
+
 int	find_i_red(t_io_red *redirection)
 {
 	t_io_red	*temp;
-	int	input_fd;
+	int	input_fd[2];
 
 	temp = redirection;
-	input_fd = 0;
+	input_fd[0] = 0;
 	if (temp == NULL)
 		return (0);
 	while (temp != NULL)
 	{
-		if (temp->in_or_out == INPUT)
+		if (temp->in_or_out == 5)
 		{
-			if (input_fd)
-				close(input_fd);
-			input_fd = open(temp->file, O_RDONLY);
+			if (input_fd[0])
+				close(input_fd[0]);
+			input_fd[0] = open(temp->file, O_RDONLY);
+		}
+		if (temp->in_or_out == 4)
+		{
+			if (input_fd[0])
+				close(input_fd[0]);
+			pipe(input_fd);
+			get_heredoc(input_fd, temp->file);
+			close(input_fd[1]);
 		}
 		temp = temp->next;
 	}
-	return (input_fd);
+	return (input_fd[0]);
 }
 
 void	copy(int buff_fd, int *o_fd, int size)
